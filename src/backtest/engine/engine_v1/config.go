6cd747b618f3ef91ab89fe106e3248e74c840f90
sirily11 <32106111+sirily11@users.noreply.github.com>
2025-04-01 00:32:46 +0800

    fix: add date range for readAll and count (#5)

--- src/backtest/engine/engine_v1/config.go
+++ src/backtest/engine/engine_v1/config.go
@@ -3,27 +3,52 @@ package engine
 import (
 	"time"
 
+	"github.com/moznion/go-optional"
 	"github.com/sirily11/argo-trading-go/src/backtest/engine/engine_v1/commission_fee"
 )
 
 type BacktestEngineV1Config struct {
-	InitialCapital float64               `yaml:"initial_capital"`
-	Broker         commission_fee.Broker `yaml:"broker"`
-	ResultsFolder  string                `yaml:"results_folder"`
-	StartTime      time.Time             `yaml:"start_time"`
-	EndTime        time.Time             `yaml:"end_time"`
+	InitialCapital float64                    `yaml:"initial_capital"`
+	Broker         commission_fee.Broker      `yaml:"broker"`
+	StartTime      optional.Option[time.Time] `yaml:"start_time"`
+	EndTime        optional.Option[time.Time] `yaml:"end_time"`
 }
 
 type BacktestEngineV1Fees struct {
 	CommissionFormula string `yaml:"commission_formula"`
 }
 
+// UnmarshalYAML implements custom unmarshaling for BacktestEngineV1Config
+func (c *BacktestEngineV1Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	type Config struct {
+		InitialCapital float64               `yaml:"initial_capital"`
+		Broker         commission_fee.Broker `yaml:"broker"`
+		StartTime      *time.Time            `yaml:"start_time"`
+		EndTime        *time.Time            `yaml:"end_time"`
+	}
+
+	var config Config
+	if err := unmarshal(&config); err != nil {
+		return err
+	}
+
+	c.InitialCapital = config.InitialCapital
+	c.Broker = config.Broker
+	if config.StartTime != nil {
+		c.StartTime = optional.Some(*config.StartTime)
+	}
+	if config.EndTime != nil {
+		c.EndTime = optional.Some(*config.EndTime)
+	}
+
+	return nil
+}
+
 func TestConfig(startTime time.Time, endTime time.Time, broker commission_fee.Broker) BacktestEngineV1Config {
 	return BacktestEngineV1Config{
 		InitialCapital: 10000,
 		Broker:         broker,
-		ResultsFolder:  "results",
-		StartTime:      startTime,
-		EndTime:        endTime,
+		StartTime:      optional.Some(startTime),
+		EndTime:        optional.Some(endTime),
 	}
 }
